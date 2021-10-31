7d911e8381262fef38a44cda9456ad0772468d4e
alfagare <alfagare@live.com>
2021-10-31 08:09:22 -0500

    Reservas no esta implementada, cambiar por opcion experiencias tipo tripadvisior

---
+++ js/reservas.js
@@ -0,0 +1,25 @@
+
+let celdas= document.getElementsByTagName("th");
+let texto_h2 =document.querySelectorAll("h2");
+
+const procesarInformation = ()=>{
+	let formulario = document.getElementById("formulario_reserva");
+	let input_cliente=formulario.cliente;
+	
+	let reserva = {}:
+	
+	reserva ['cliente']=formulario.cliente.value;
+	reserva ['documento']=formulario.documento.value;
+	let salon =formulario.
+	reserva ['cliente']
+	
+	console.log(input_cliente);
+	alert(texto_h2);
+	
+};
+
+
+document.write(texto_h2);
+
+console.log(texto_h2);
+
